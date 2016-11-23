3fb7eee76888de25e135fddb0b10903f3a19cc3a
Christina Zhu <cszhu@ucdavis.edu>
2016-11-22 16:04:42 -0800

    List basic open issues for user

--- src/FeedbackBot/Controllers/HomeController.cs
+++ src/FeedbackBot/Controllers/HomeController.cs
@@ -3,19 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Octokit;
 
 namespace FeedbackBot.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var client = new GitHubClient(new ProductHeaderValue("FeedbackBot"));
+            var basicAuth = new Credentials("", ""); // NOTE: not real credentials
+            client.Credentials = basicAuth;
+            var user = await client.User.Current();
+            var recently = new IssueRequest
+            {
+                Filter = IssueFilter.Created,
+                Since = DateTimeOffset.Now.Subtract(TimeSpan.FromDays(14))
+            };
+            var issues = await client.Issue.GetAllForCurrent(recently);
+
+            ViewData["Message"] = "Current Feedback";
+            return View(issues);
         }
+
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Your application descriptieeeeeeon page.";
 
             return View();
         }
--- src/FeedbackBot/wwwroot/js/site.js
+++ src/FeedbackBot/wwwroot/js/site.js
@@ -1,1 +1,15 @@
 ﻿// Write your Javascript code.
+console.log("hello!!!");
+
+$.ajax({
+    type: "GET",
+    url: "https://api.github.com/repos/ucdavis/FeedbackBot/issues/1",
+    dataType: "json",
+    success: function(data) {
+        // Debugging purposes
+        console.log(data);
+    },
+    error: function(data) {
+        console.log(data);
+    }
+  });
