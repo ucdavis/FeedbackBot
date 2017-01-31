1ff3f6f8fd23be396f7ded9e078cf7143f785348
Christina Zhu <cszhu@ucdavis.edu>
2017-01-31 13:14:54 -0800

    christinaVoting (#21)
    * preliminary voting working
    
    * Comment basic functionality

--- src/FeedbackBot/wwwroot/_references.js
+++ src/FeedbackBot/wwwroot/_references.js
@@ -2,5 +2,5 @@
 /// <reference path="js/site.js" />
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
 /// <reference path="lib/jquery/dist/jquery.js" />
-/// <reference path="lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js" />
+/// <reference path="lib/jquery-validation-unobtrusive/dist/jquery.validate.unobtrusive.js" />
 /// <reference path="lib/tether/dist/js/tether.js" />
