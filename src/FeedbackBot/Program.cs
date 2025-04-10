27ae3cd44140c4555efeb4d7913cffc9744cb373
Jason Sylvestre <jsylvestre@ucdavis.edu>
2025-04-10 07:41:39 -0700

    User secrets

--- src/FeedbackBot/Program.cs
+++ src/FeedbackBot/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 namespace FeedbackBot
@@ -13,6 +14,14 @@ namespace FeedbackBot
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    var env = context.HostingEnvironment;
+                    if(env.IsDevelopment())
+                    {
+                        config.AddUserSecrets<Program>();
+                    }
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
