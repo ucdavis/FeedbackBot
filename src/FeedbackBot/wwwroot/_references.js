d52830210124ed6a68275bbb6972fb711adb2f09
Cal <cydoval@mac.com>
2026-06-10 12:05:20 -0700

    cleanout

--- src/FeedbackBot/wwwroot/_references.js
+++
@@ -1,7 +0,0 @@
-﻿/// <autosync enabled="true" />
-/// <reference path="../views/shared/css_browser_selector.js" />
-/// <reference path="js/site.js" />
-/// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
-/// <reference path="lib/jquery/dist/jquery.js" />
-/// <reference path="lib/jquery-validation-unobtrusive/dist/jquery.validate.unobtrusive.js" />
-/// <reference path="lib/tether/dist/js/tether.js" />
